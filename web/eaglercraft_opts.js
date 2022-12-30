0031da27227139d383d78008de2dcde1908bda7d
Sylvia <92600501+sylvxa@users.noreply.github.com>
2022-12-29 20:27:22 -0600

    Update eaglercraft_opts.js

--- web/eaglercraft_opts.js
+++ web/eaglercraft_opts.js
@@ -3,8 +3,8 @@ window.eaglercraftXOpts = () => ({
   // joinServer: "ws" + window.location.href.slice(4),
   // assetsURI: "custom.epk",
 	servers: [
-		{ addr: "ws" + window.location.href.slice(4), name: "EaglercraftX Server" }
+		{ addr: "wss://mc.blahaj.tk/", name: "JOIN HERE" }
 	]
 });
-// document.title = "My Original Server Name:tm:";
+document.title = "made with <3 by sylvxa";
 // document.querySelector("link[rel=\"shortcut icon\"]").href = "https://www.google.com/favicon.ico";
